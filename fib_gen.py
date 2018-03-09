1fc7a594a8fa74afea8094859e0bc8af20bf8723
ubdussamad <samadhaque23@gmail.com>
2018-03-09 16:47:06 +0530

    Update fib_gen.py

--- fib_gen.py
+++ fib_gen.py
@@ -8,14 +8,16 @@ def timeit(func):
 	return time_wrapper
 
 
-@timeit
 def fibonacci():
     a, b = 0, 1
     while True:
         yield a
         a, b = b, a + b
+@timeit
+def main():
+        for j,i in enumerate(fibonacci()):
+                if i>10**999:
+                        print('The index is:',j,',and the value at that index is:',i)
+                        break
 
-for j,i in enumerate(fibonacci()):
-    if i>10**999:
-        print('The index is:',j,',and the value at that index is:',i)
-        break
+main()
